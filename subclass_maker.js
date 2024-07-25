86b4c8be234138f1a5e4a983c71a7e3e753dfac8
Andreas Barbre <ao.barbre@gmail.com>
2024-07-25 16:04:30 +0200

    more stuff

--- subclass_maker.js
+++ subclass_maker.js
@@ -1,20 +1,109 @@
+import { input, select } from "@inquirer/prompts";
+import fs from "node:fs";
+import path from "node:path";
 
-import select, { Separator } from '@inquirer/select'
+const json = fs.readFileSync(
+  path.join(import.meta.dirname, "/Tales_of_Lumeeria_Core Update thread.json"),
+  "utf8",
+  (err, data) => data
+);
 
-const classes = [
-    "Teddy",
-    "Fighter",
-    "Darth Vader"
-]
+let homebrew = JSON.parse(json);
 
+let classes = [
+  {
+    name: "Artificer",
+    source: "tce",
+  },
+  {
+    name: "Barbarian",
+    source: "phb",
+  },
+  {
+    name: "Bard",
+    source: "phb",
+  },
+  {
+    name: "Cleric",
+    source: "phb",
+  },
+  {
+    name: "Druid",
+    source: "phb",
+  },
+  {
+    name: "Fighter",
+    source: "phb",
+  },
+  {
+    name: "Monk",
+    source: "phb",
+  },
+  {
+    name: "Paladin",
+    source: "phb",
+  },
+  {
+    name: "Ranger",
+    source: "phb",
+  },
+  {
+    name: "Rogue",
+    source: "phb",
+  },
+  {
+    name: "Sorcerer",
+    source: "phb",
+  },
+  {
+    name: "Warlock",
+    source: "phb",
+  },
+  {
+    name: "Wizard",
+    source: "phb",
+  },
+];
+
+let homebrewClasses = homebrew.class.map((x) => {
+  const object = {
+    name: x.name,
+    source: x.source,
+  };
+  return object;
+});
+
+const mergeList = (x, y) => {
+  y.forEach((element) => {
+    x.push(element);
+  });
+  return x;
+};
+
+classes = mergeList(classes, homebrewClasses);
+
+console.log(classes);
 const mainClass = await select({
-    message: "Choose your Main Class",
-    choices: classes.map( x => {
-        const temp = {
-            name: x,
-            value: `${x} value`,
-        }
-        return temp
-    })
-})
-console.log(mainClass)
+  message: "What the main class?",
+  choices: classes.map((x) => {
+    const object = {
+      name: x.name,
+      value: x,
+    };
+    return object;
+  }),
+  loop: false,
+});
+
+const subclassName = await input({ message: "Give your subclass a name:" });
+
+const subclassSource = await select({
+  message: "What the source of the subclass?",
+  choices: homebrew._meta.sources.map((source) => {
+    const object = {
+      name: source.full,
+      value: source.json,
+    };
+  }),
+});
+console.log(mainClass);
